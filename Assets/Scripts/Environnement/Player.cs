cf9e75d29db674c7c7717e9951386f0674436e2d
Romain D <36967424+Rhuminoux@users.noreply.github.com>
2022-01-24 11:26:03 +0100

    Commit Asset

---
+++ Assets/Scripts/Environnement/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Sprite iddleJoe;
+    public Sprite cleaningJoe;
+
+    public Manager gameManager;
+    public GameObject deathMenu;
+    public Nacelle nacelle;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("Manager").GetComponent<Manager>();
+        deathMenu = gameManager.deathMenu;
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.StartsWith("Dirt"))
+        {
+            nacelle._score += 10;
+            nacelle.scoreText.text = nacelle._score.ToString();
+            Destroy(collision.gameObject);
+            gameObject.GetComponent<SpriteRenderer>().sprite = cleaningJoe;
+            StartCoroutine(SetSprite());
+        }
+        else if (collision.tag.StartsWith("Rock"))
+        {
+            deathMenu.SetActive(true);
+            gameManager.SetStateGameMenu();
+        }
+    }
+
+    IEnumerator SetSprite()
+    {
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<SpriteRenderer>().sprite = iddleJoe;
+    }
+}
